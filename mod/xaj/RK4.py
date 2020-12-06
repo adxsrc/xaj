ea9d9ea75a4f84fc1dd76c666641982ffba85fc7
Chi-kwan Chan <chanc@arizona.edu>
2020-12-06 07:55:30 -0700

    Move RK4 from `pray`

--- mod/xaj/RK4.py
+++ mod/xaj/RK4.py
@@ -17,4 +17,9 @@
 # along with XAJ.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from .RK4 import RK4
+def RK4(rhs, state, dt):
+    k1 = dt * rhs(state           )
+    k2 = dt * rhs(state + 0.5 * k1)
+    k3 = dt * rhs(state + 0.5 * k2)
+    k4 = dt * rhs(state +       k3)
+    return state + k1 / 6 + k2 / 3 + k3 / 3 + k4 / 6
