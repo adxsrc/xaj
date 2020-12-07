9af9c874ef3582fb00197b9874f0e7514a08a6cf
Chi-kwan Chan <chanc@arizona.edu>
2020-12-06 21:55:19 -0700

    Restructure how the DP5 integrator can be used

--- mod/xaj/DP5.py
+++ mod/xaj/DP5.py
@@ -33,31 +33,40 @@ class DP5:
     e  = np.array([71/57600,    0,         -71/16695,    71/1920, -17253/339200, 22/525, -1/40])
 
     def __init__(self, rhs):
+        # Required
+        self.rhs = rhs
 
-        def step(x, y, h, k6): # closure on coefficients and rhs
+        # Internal states
+        self.x  = None
+        self.y  = None
+        self.k6 = None
 
-            xch = x + self.c  * h
-            a1h =     self.a1 * h
-            a2h =     self.a2 * h
-            a3h =     self.a3 * h
-            a4h =     self.a4 * h
-            a5h =     self.a5 * h
-            a6h =     self.a6 * h
-            eh  =     self.e  * h
+    def setup(self, x, y): # may be xmapped
+        self.x  = x
+        self.y  = y
+        self.k6 = self.rhs(x, y)
 
-            k0  = k6 # of previous step, i.e., == rhs(x, y) for this step
-            k1  = rhs(xch[1], y + a1h[0]*k0)
-            k2  = rhs(xch[2], y + a2h[0]*k0 + a2h[1]*k1)
-            k3  = rhs(xch[3], y + a3h[0]*k0 + a3h[1]*k1 + a3h[2]*k2)
-            k4  = rhs(xch[4], y + a4h[0]*k0 + a4h[1]*k1 + a4h[2]*k2 + a4h[3]*k3)
-            k5  = rhs(xch[5], y + a5h[0]*k0 + a5h[1]*k1 + a5h[2]*k2 + a5h[3]*k3 + a5h[4]*k4)
-            Y   =             y + a6h[0]*k0 +             a6h[2]*k2 + a6h[3]*k3 + a6h[4]*k4 + a6h[5]*k5
-            k6  = rhs(xch[5], Y) # will be used in the next step, hence the output
-            E   =                 eh [0]*k0 +             eh [2]*k2 + eh [3]*k3 + eh [4]*k4 + eh [5]*k5 + eh [6]*k6
+    def step(self, h): # may be xmapped, would not change internal states
+        xch = self.x + self.c  * h
+        a1h =          self.a1 * h
+        a2h =          self.a2 * h
+        a3h =          self.a3 * h
+        a4h =          self.a4 * h
+        a5h =          self.a5 * h
+        a6h =          self.a6 * h
+        eh  =          self.e  * h
 
-            return DP5Output(x, y, h, k0, k2, k3, k4, k5, k6, Y), Y, E, k6
+        k0  = self.k6 # of previous step, i.e., == rhs(x, y) for this step
+        k1  = self.rhs(xch[1], self.y + a1h[0]*k0)
+        k2  = self.rhs(xch[2], self.y + a2h[0]*k0 + a2h[1]*k1)
+        k3  = self.rhs(xch[3], self.y + a3h[0]*k0 + a3h[1]*k1 + a3h[2]*k2)
+        k4  = self.rhs(xch[4], self.y + a4h[0]*k0 + a4h[1]*k1 + a4h[2]*k2 + a4h[3]*k3)
+        k5  = self.rhs(xch[5], self.y + a5h[0]*k0 + a5h[1]*k1 + a5h[2]*k2 + a5h[3]*k3 + a5h[4]*k4)
+        Y   =                  self.y + a6h[0]*k0 +             a6h[2]*k2 + a6h[3]*k3 + a6h[4]*k4 + a6h[5]*k5
+        k6  = self.rhs(xch[5], Y) # will be used in the next step, hence the output
+        E   =                           eh [0]*k0 +             eh [2]*k2 + eh [3]*k3 + eh [4]*k4 + eh [5]*k5 + eh [6]*k6
 
-        self.step = step
+        return Y, E, (k0, k1, k2, k3, k4, k5, k6)
 
 
 class DP5Output:
@@ -73,10 +82,11 @@ class DP5Output:
         +69997945/29380423,
     ])
 
-    def __init__(self, x, y, h, k0, k2, k3, k4, k5, k6, Y):
+    def __init__(self, x, X, y, Y, k):
 
+        h     = X - x
         ydiff = Y - y
-        bspl  = k0 * h - ydiff
+        bspl  = k[0] * h - ydiff
         dh    = self.d * h
 
         self.x  = x
@@ -84,8 +94,8 @@ class DP5Output:
         self.r0 = y
         self.r1 = ydiff
         self.r2 = bspl
-        self.r3 = ydiff - k6 * h - bspl
-        self.r4 = dh[0]*k0 + dh[2]*k2 + dh[3]*k3 + dh[4]*k4 + dh[5]*k5 + dh[6]*k6
+        self.r3 = ydiff - k[6] * h - bspl
+        self.r4 = dh[0]*k[0] + dh[2]*k[2] + dh[3]*k[3] + dh[4]*k[4] + dh[5]*k[5] + dh[6]*k[6]
 
     def denseout(self, x):
         s  = (x - self.x) / self.h
