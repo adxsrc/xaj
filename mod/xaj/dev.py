08699db45a399d9b0514f6ff450cd18e8e457a12
Chi-kwan Chan <chanc@arizona.edu>
2021-12-20 22:49:21 -0700

    Implement a compact form of DP5 stepper

--- mod/xaj/dev.py
+++ mod/xaj/dev.py
@@ -15,3 +15,32 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with XAJ.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from jax import numpy as np
+
+
+def DP5(rhs):
+
+    c = {0:    0.0,    1:     1/5,    2:    3/10,    3:   4/5,    4:     8/9,      5: 1.0,   6: 1.0 }
+    a = {
+      0:{                                                                                           },
+      1:{0:    1/5                                                                                  },
+      2:{0:    3/40,   1:     9/40                                                                  },
+      3:{0:   44/45,   1:-   56/15,   2:   32/9                                                     },
+      4:{0:19372/6561, 1:-25360/2187, 2:64448/6561,  3:-212/729                                     },
+      5:{0: 9017/3168, 1:-  355/33,   2:46732/5247,  3:  49/176,  4:- 5103/18656                    },
+      6:{0:   35/384,                 2:  500/1113,  3: 125/192,  4:- 2187/6784,   5:11/84          },
+    }
+    e = {0:   71/57600,               2:-  71/16695, 3:  71/1920, 4:-17253/339200, 5:22/525, 6:-1/40}
+
+    def step(x, y, h, k): # closure on rhs, may be xmapped
+        K = [] if k is None else k[-1:]
+        for i in range(len(K),7):
+            X = x + h * c[i]
+            Y = y + h * sum(v * K[j] for j, v in a[i].items())
+            K.append(rhs(X, Y))
+        E = h * sum(v * K[j] for j, v in e.items())
+        return Y, E, K
+
+    return step
