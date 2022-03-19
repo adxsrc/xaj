0c010765dc1f7d6612cd50e0d730eb2e0dca7d6d
Chi-kwan Chan <chanc@arizona.edu>
2022-03-19 00:18:31 -0700

    Bump XAJ version to 0.1.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xaj',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/adxsrc/xaj',
     author='Chi-kwan Chan',
     author_email='chanc@arizona.edu',
