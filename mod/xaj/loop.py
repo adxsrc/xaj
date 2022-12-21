cacba30fe624b1f4cce69e07a2b51cae06fc018e
Chi-kwan Chan <chanc@arizona.edu>
2022-12-20 21:07:11 -0700

    Abstract the engine problem into a loop problem

---
+++ mod/xaj/loop.py
@@ -0,0 +1,37 @@
+# Copyright 2022 Chi-kwan Chan
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""scan(), a conditional lux.scan() with early termination"""
+
+
+def scan(func, carry, scanees=None, filt=None, length=None, reverse=False, unroll=1):
+    """An improved `jax.lax.scan()`
+
+    `jax.lax` provides two different loops, `while_loop()` and
+    `scan()`.  The first one can terminate dynamically depending on
+    `cond()` but is not capable to create output at each step.  The
+    second one, on the other hand, can store output at each step but
+    is not capable to terminate dynamically.
+
+    For ODE integration, the evaluation of `rhs()` is usually
+    expensive and we are interested in outputting every step.  Hence,
+    it is necessary to combine the features of `jax.lax.while_loop()`
+    and `jax.lax.scan()`.
+
+    xaj.loop.scan() is exactly such a function that allows early
+    termination.  In addition, it can filter out unwanted output to
+    reduce memory usage.
+
+    """
